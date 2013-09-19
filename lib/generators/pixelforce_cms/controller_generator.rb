255234e8718f5e60ca4036075ffeae9a4321564e
Ben Zhang <bzbnhang@gmail.com>
2013-09-19 18:01:36 +0930

    add more templates

--- lib/generators/pixelforce_cms/controller_generator.rb
+++ lib/generators/pixelforce_cms/controller_generator.rb
@@ -11,6 +11,13 @@ module PixelforceCms
         template "admin_controller.rb", "app/controllers/admin/#{file_name.pluralize}_controller.rb"
       end
 
+      def create_views
+        # template 'index.html.haml', "app/views/admin/#{file_name}/index.html.haml"
+        template 'new.html.haml', "app/views/admin/#{file_name}/new.html.haml"
+        template 'edit.html.haml', "app/views/admin/#{file_name}/edit.html.haml"
+        template '_form.html.haml', "app/views/admin/#{file_name}/_form.html.haml"
+      end
+
       def create_route
         sentinel = "authenticated :user do\n"
         routing_code = "resources :#{file_name.pluralize}, controller: 'admin/#{file_name.pluralize}'"
@@ -22,7 +29,7 @@ module PixelforceCms
       private
 
       def file_name
-        controller_name.underscore
+        controller_name.pluralize.underscore
       end
     end
   end
--- lib/pixelforce_cms/version.rb
+++ lib/pixelforce_cms/version.rb
@@ -1,3 +1,3 @@
 module PixelforceCms
-  VERSION = "0.3"
+  VERSION = "0.4"
 end
