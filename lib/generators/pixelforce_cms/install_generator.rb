fa3194bf5293b97a8a93e28ae9ffcb6dc6b05f4e
Ben Zhang <bzbnhang@gmail.com>
2013-09-27 22:59:12 +0930

    add page controller

--- lib/generators/pixelforce_cms/install_generator.rb
+++ lib/generators/pixelforce_cms/install_generator.rb
@@ -23,10 +23,12 @@ module PixelforceCms
       end
 
       def copy_controller_files
+        copy_file "pages_controller.rb", 'app/controllers/pages_controller.rb'
         copy_file "application.html.haml", 'app/views/layouts/application.html.haml'
         create_file 'app/views/pages/index.html.haml'
         in_root do
           inject_into_file 'config/routes.rb', "\n  root :to => 'pages#index'\n", { :after => 'do', :verbose => false }
+          inject_into_file 'config/routes.rb', "\n  get '/:id' => 'pages#show'\n", { :after => 'do', :verbose => false }
         end
       end
 
--- lib/pixelforce_cms/version.rb
+++ lib/pixelforce_cms/version.rb
@@ -1,3 +1,3 @@
 module PixelforceCms
-  VERSION = "0.4"
+  VERSION = "0.5"
 end
